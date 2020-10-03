89f10346eea95915ac29a42ae47091678840a3c9
szyxiii <x7174nx@gmail.com>
2020-10-03 16:55:26 +0300

    Minor changes

--- js/main.js
+++ js/main.js
@@ -30,17 +30,24 @@ var facts = [
     "When creating this site, the Bootstrap documentation was worn down to holes.",
     "I re-create my GitHub repositories so that no one will see my stupid mistakes in commits history.",
     "No, it's not a rIdIcUloUs SlOw PRoGRamMiNg LanGuAgE, it's you who are using it wrong.",
-    "Vkontakte even has bugs on the page with the bug report.",
-    "#JuniorLivesMatter",
+    "Vkontakte even has bugs on the page with the bug report, can you believe that?",
+    "<a href=\"https://www.urbandictionary.com/define.php?term=Programmer\" target=\"_\">#ProgrammersLivesMatter</a>",
     "Beep-bop bop. boop.",
     "Only a Sith deals in absolutes.",
     "Done is better than perfect.",
     "You can't read my nickname properly.",
-    "You know... ah... forget 'bout it..."
+    "You know... ah... forget 'bout it...",
+    "I graduated from high school, am I senior?",
+    "Write first, then refactor.",
+    "[W] PEP 8 (E261): at least two spaces before inline comment.",
+    "I don't know, check on <a href=\"https://python.org\" target=\"_\">python.org</a>",
+    "Oh gosh. I think I forgot to begin transaction before modifying.............",
+    "How tf autocommit works in pymysql?!",
+    'INSERT INTO `users` VALUES (%, %, %)'
 ];
 
 function showRandomFact() {
-    var fact = '/Fact text here/';
+    var fact = '';
 
     if(Math.random() > 0.1)
         fact = facts[random(facts.length)];
